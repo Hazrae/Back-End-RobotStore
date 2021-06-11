5ffff19e0e27a12bdb77f34e476f709ba9bd9922
IraVehementi <kevinvoneche@gmail.com>
2021-06-11 17:42:06 +0200

    CORS - user

--- RobotAPI/Controllers/UserController.cs
+++ RobotAPI/Controllers/UserController.cs
@@ -7,9 +7,11 @@ using System.Web.UI;
 using DALRobot.Services;
 using RobotAPI.Tools;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace RobotAPI.Controllers
 {
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
     public class UserController : ApiController
     {
         private UserService userServ = new UserService();
